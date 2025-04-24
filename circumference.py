b0a2bd29680f3a8300bb4c869480dc52c43b8528
suhana0709 <farjanajaman1975@gmail.com>
2025-04-24 09:49:09 -0500

    Add files via upload

---
+++ circumference.py
@@ -0,0 +1,6 @@
+def circumference(p, radius):
+    return 2*p*radius
+p = 3.142
+print("Circumference Calculator:")
+radius = int(input("Enter the radius of the circle: "))
+print("\n Circumference: ", circumference(p, radius))
