21422b47884eb16ad0167a67b98dba6558c2713d
suhana0709 <farjanajaman1975@gmail.com>
2025-04-25 08:23:17 -0500

    Add files via upload

---
+++ shutdown.py
@@ -0,0 +1,6 @@
+#actually can shut the the whole computer system!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+import os
+
+# Shut down the computer (Windows)
+os.system("shutdown /s /t 0")
