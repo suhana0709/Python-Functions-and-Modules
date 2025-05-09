4f27ef46e8e982fb0191b3f9dbac51512d2cf60e
suhana0709 <farjanajaman1975@gmail.com>
2025-05-09 08:48:24 -0500

    Add files via upload

---
+++ trigonometric_value.py
@@ -0,0 +1,10 @@
+import math
+
+num = int(input("Enter the angle: "))
+sin = math.sin(num)
+cos = math.cos(num)
+tan = math.tan(num)
+
+print("\n sin = ",sin)
+print("cos = ",cos)
+print("tan = ",tan)
